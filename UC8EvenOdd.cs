6e9de5c193ccffd0d32e1a11733c55503a85928e
AbhiShinde1 <abhishinde572@gmail.com>
2022-08-30 22:02:42 +0530

    [Abhishek] EvenOdd Program

--- UC8EvenOdd.cs
+++ UC8EvenOdd.cs
@@ -8,5 +8,18 @@ namespace basic
 {
     internal class UC8EvenOdd
     {
+        public static void evenOdd()
+        {
+            Console.WriteLine("enter any number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine(num + " is even number");
+            }
+            else
+            {
+                Console.WriteLine(num + " is odd number");
+            }
+        }
     }
 }
