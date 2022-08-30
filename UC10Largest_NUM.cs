b80b4ea507436cbaa1ef4e576b173b7609c06869
AbhiShinde1 <abhishinde572@gmail.com>
2022-08-30 22:11:44 +0530

    [Abhishek] LargestNum Program

--- UC10Largest_NUM.cs
+++ UC10Largest_NUM.cs
@@ -8,5 +8,35 @@ namespace basic
 {
     internal class UC10Largest_NUM
     {
+        public static void findlargestNumber()
+        {
+            int num1, num2, num3;
+            Console.WriteLine("Enter first number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter third number");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.WriteLine("{0} is Largest", num1);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is Largest", num3);
+                }
+            }
+            else if (num2 > num3)
+            {
+                Console.WriteLine("{0} is Largest", num2);
+            }
+            else
+            {
+                Console.WriteLine("{0} is Largest", num3);
+            }
+        }
     }
 }
