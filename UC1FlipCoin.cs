bc8605748b71cdd55ca78cf202b1cf554744a6c0
AbhiShinde1 <abhishinde572@gmail.com>
2022-08-30 21:07:47 +0530

    [Abhishek] FlipCoin Program

---
+++ UC1FlipCoin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic
+{
+    internal class UC1FlipCoin
+    {
+       
+            public static void FlipCoin()
+            {
+                int heads = 0, tails = 0;
+                Console.WriteLine("Enter how many times want to flip coin");
+                int n = Convert.ToInt32(Console.ReadLine());
+                Random random = new Random();
+
+                for (int i = 0; i <= n; i++)
+                {
+                    double randomNum = random.NextDouble();
+                    Console.WriteLine("random input" + randomNum);
+                    if (randomNum <= 0.5)
+                    {
+                        if (randomNum == 0)
+                        {
+                            Console.WriteLine("Heads");
+                            heads += 1;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Tails");
+                            tails += 1;
+                        }
+                    }
+                }
+                Console.WriteLine("Total number of Heads:{0} and Tails{1}", heads, tails);
+            }
+        }
+    }
+
