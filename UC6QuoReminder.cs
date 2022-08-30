4b91189e8922d199557564ddc708e4c14ff6f450
AbhiShinde1 <abhishinde572@gmail.com>
2022-08-30 21:41:53 +0530

    [Abhishek] QuoReminder Program

---
+++ UC6QuoReminder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic
+{
+    internal class UC6QuoReminder
+    {
+        public static void QuoReminder()
+        {
+            Console.WriteLine("Enter the value of dividend");
+            int numdividend = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of divisor");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+
+            int numquotient = numdividend / divisor;
+            int numremainder = numdividend % divisor;
+            Console.WriteLine("quotient = " + numquotient);
+            Console.WriteLine("remainder = " + numremainder);
+        }
+    }
+}
+
